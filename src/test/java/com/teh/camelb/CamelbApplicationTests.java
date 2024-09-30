7e8d0a1342623c8683f81eb15612aa8a3d81f381
Luiz Fernando Areas <lfernandoareas@gmail.com>
2024-09-30 06:37:46 -0300

    first commit camel microservice B

---
+++ src/test/java/com/teh/camelb/CamelbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teh.camelb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
